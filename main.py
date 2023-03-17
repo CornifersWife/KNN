e5cb289dc3bdb41a6547e1e084332ef85ce074f8
CornifersWife <irys.latosz@gmail.com>
2023-03-17 21:40:52 +0100

    knn works fully, can add new lines by user, add excel, check for the other data

--- main.py
+++ main.py
@@ -8,29 +8,27 @@ def read_csv(filename):
         all_data = []
         for row in csv_reader:
             row_data = [float(element) for element in row[:-1]]
-            row_data += [row[-1]]  # add last element as string
-            all_data.append(row_data)  # add row_data to list
+            row_data += [row[-1]]
+            all_data.append(row_data)
     return all_data
 
 
-def euclydian_distance(coordinates):
+def euclidean_distance(coordinates):
     sum = 0
     for value in coordinates[:-1]:
         sum += value ** 2
     return math.sqrt(sum)
 
 
-def knn(k, train_data, test_row):
+def single_row_knn(train_set, test_row, k):
     data_tmp = []
-    for row in train_data:
+    for row in train_set:
         relative_cords = []
         for column in range(len(row) - 1):
             relative_cords.append(row[column] - test_row[column])
-        distance = euclydian_distance(relative_cords)
+        distance = euclidean_distance(relative_cords)
         data_tmp.append((distance, row[-1]))
-
     data_tmp.sort(key=lambda x: x[0])
-
     map = {}
     for element in data_tmp[:k]:
         name = element[1]
@@ -38,26 +36,42 @@ def knn(k, train_data, test_row):
             map[name] += 1
         else:
             map[name] = 1
-    for key,value in map.items():
+    for key, value in map.items():
         print(f'{key}: {value}')
-    return max(map, key=lambda x: map[x])
+    return max(map, key=lambda x: map[x]) == test_row[-1]
+    """
+    z racji że max w momencie gdy ma wybrać spośród równych sobie wartości 
+    to wybiera najwcześniej dodaną najwcześniej to dokładność 
+    jest często większa niż spoedziewana
+    """
 
 
-data_in = read_csv('data/iris.data')
-data_test = read_csv('data/iris.test.data')
-k = len(data_in)
+def knn(train_set, test_set, k):
+    total = 0
+    correct = 0
+    for test_row in test_set:
+        total += 1
+        if (single_row_knn(train_set, test_row, k)):
+            correct += 1
+    return correct, total
 
-total = 0
-correct = 0
-for row in data_test:
-    print (f'\nCorrect is:  {row[-1]}')
-    total += 1
-    name = knn(k, data_in, row)
-    print(f'I guessed: {name}')
-    if name == row[-1]:
-        print('yas')
-        correct += 1
-    else:
-        print(':(')
-        # print(f' guessed {knn(k, data_in, row)} should be {row[-1]};')
-print(f'Accuracy is {correct * 100 / total}%')
+
+def Accuracy(correct, total):
+    print(f'Accuracy is {correct * 100 / total}%')
+
+
+train_set = read_csv('data/iris.data')
+test_set = read_csv('data/iris.test.data')
+k = len(train_set)
+correct, total = knn(train_set, test_set, k)
+Accuracy(correct, total)
+vector = train_set[0]
+formatted_vector = ", ".join([f"{elem}" for elem in vector[:-1]]) + f", {vector[-1]}"
+print(f"Input singular vector written in the same vector as \n{formatted_vector}")
+while True:
+    input_vector_str = input(':')
+    input_vector_str = input_vector_str.replace(" ", "").replace("\t", "").replace("\n", "")
+    input_vector = input_vector_str.split(',')
+    input_vector[:-1] = [float(x) for x in input_vector[:-1]]
+    output = "correctly" if single_row_knn(train_set,input_vector,k) else "incorrectly"
+    print(f'For this vector the program has guessed {output}')
